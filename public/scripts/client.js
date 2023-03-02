8332e23a85db288a2865bbd2c0b3d1bae898f2a8
cniscoding <asdfa.com>
2023-03-01 22:49:17 -0800

    textarea input updating without refreshing page. prepend to tweet container working.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,50 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const tweetData = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
 $(document).ready(function() {
 
-  console.log('client.JS - page is ready');
   const loadTweets = () => {
     $.ajax({
         method: 'GET',
         url: 'http://localhost:8080/tweets',
         success: (response) => {
           $('#tweet-container').empty();
-          // does it matter if it's console.log or return?
-          return renderTweets(response);
+          renderTweets(response);
         }
       })
   };
 
   loadTweets();
   
-  
-  // $('#tweet-container').empty() when doing ajax
   const renderTweets = function(tweets) {
     // loops through tweets
     for (const tweet of tweets) {
@@ -56,10 +27,10 @@ $(document).ready(function() {
       // takes return value and appends it to the tweets container
       const $tweetComplete = createTweetElement(tweet);
       
-      $('#tweet-container').append($tweetComplete);
+      $('#tweet-container').prepend($tweetComplete);
     }
   };
-  //that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
+  
   createTweetElement = function(tweet) {
     const $tweetTemp = `
       <article>
@@ -77,7 +48,7 @@ $(document).ready(function() {
       </div>
       <footer>
         <div>
-          <p>timeago.format(${tweet.created_at})</p>
+          <p>${timeago.format(tweet.created_at)}</p>
         </div>
         <div>
           <i class="fa-solid fa-flag"></i>
@@ -99,11 +70,10 @@ $(document).ready(function() {
     console.log(urlEncoded)
     $.ajax({
       method:'POST',
-      // url: '../../server/index.js',
+      url: 'http://localhost:8080/tweets',
       data :urlEncoded,
-      sucess: (response) => {
+      success: (response) => {
         console.log(response);
-        // $('#tweet-container').empty();
         loadTweets();
       }
     })
