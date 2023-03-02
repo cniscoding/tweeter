d3d5111d755c61fb07e005cadcf10264318a885c
cniscoding <asdfa.com>
2023-03-01 21:26:13 -0800

    add data checks and validation for textarea

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 $(document).ready(function() {
+
   console.log('client.JS - page is ready');
+  const loadTweets = () => {
+    $.ajax({
+        method: 'GET',
+        url: 'http://localhost:8080/tweets',
+        success: (response) => {
+          $('#tweet-container').empty();
+          // does it matter if it's console.log or return?
+          return renderTweets(response);
+        }
+      })
+  };
+
+  loadTweets();
+  
   
   // $('#tweet-container').empty() when doing ajax
   const renderTweets = function(tweets) {
@@ -62,7 +77,7 @@ $(document).ready(function() {
       </div>
       <footer>
         <div>
-          <p>${tweet.created_at}</p>
+          <p>timeago.format(${tweet.created_at})</p>
         </div>
         <div>
           <i class="fa-solid fa-flag"></i>
@@ -75,7 +90,7 @@ $(document).ready(function() {
     return $tweetTemp;
   };
    
-  renderTweets(tweetData);
+  // renderTweets(tweetData);
 
   const $tweetForm = $('form')
   $tweetForm.on('submit', (event) => {
@@ -84,16 +99,13 @@ $(document).ready(function() {
     console.log(urlEncoded)
     $.ajax({
       method:'POST',
-      url: '../../server/index.js',
+      // url: '../../server/index.js',
       data :urlEncoded,
       sucess: (response) => {
         console.log(response);
+        // $('#tweet-container').empty();
+        loadTweets();
       }
     })
-    //get data from form
-    //urlEncoded the data
-    //make ajax post request
-
-  })
-  
+  }) 
 });
